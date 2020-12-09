5973740ac3736ad088002ca4ece5a8be57e42e57
Charles Ferreira Nunes <charles.nunes@campus.unimontes.int>
2020-12-09 10:54:43 -0300

    Coreção de erros nas migrations e mudanças nas rotas

--- database/migrations/2020_12_09_125135_create_author.php
+++ database/migrations/2020_12_09_125135_create_author.php
@@ -14,7 +14,7 @@ class CreateAuthor extends Migration
     public function up()
     {
         Schema::create('author', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 
+Route::get('/', [App\Http\Controllers\HomeController::class, 'home'])->name('index');
 
 Auth::routes();
 
@@ -22,3 +23,5 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('/posts', 'BlogPostController');
